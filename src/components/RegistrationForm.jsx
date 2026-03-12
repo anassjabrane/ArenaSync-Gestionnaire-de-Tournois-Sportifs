21bc375ab2dffb819d8f02b7514aa5ffccb1edf1
Anass Jabrane <jabraneanass12@gmail.com>
2026-03-12 12:29:32 +0000

    handle form submission and display feedback message

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -5,8 +5,16 @@ export default function RegistrationForm(){
     const [nom,SetNom]=useState("");
     const [equipe ,SetEquipe]=useState("");
     const [niveau,SetNiveau]=useState("debutent");
+    const [message, setMessage] = useState("");
 
-    
+    const isFormValid= nom.length >=3;
+
+    const handelSubmit = (e)=>{
+        e.preventDefault();
+        setMessage('bravo ${nom},tu es inscrit avec l’équipe ${equipe} ')
+    }
+
+
 
 
 }
