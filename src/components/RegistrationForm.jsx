d7b6a841469a10d1197cdb4f2614f28548e92be1
Anass Jabrane <jabraneanass12@gmail.com>
2026-03-12 12:00:16 +0000

    Add state for nom, equipe, and niveau

---
+++ src/components/RegistrationForm.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export default function RegistrationForm(){
+    // etat de formulaire 
+    const [nom,SetNom]=useState("");
+    const [equipe ,SetEquipe]=useState("");
+    const [niveau,SetNiveau]=useState("debutent");
+
+    
+
+
+}
